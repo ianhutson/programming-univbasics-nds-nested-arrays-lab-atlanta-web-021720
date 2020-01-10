0c038ea605e4c43b1fff0b9199d0c0e5f3c4e15f
Ian Hutson <ih00209@georgiasouthern.edu>
2020-01-10 19:11:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -21,8 +21,8 @@ def assembled_matrix
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
-  storage_room = [ORGANIC_PRODUCE = ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"],
-    CONVENTIONAL_PRODUCE = ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"]]
+  storage_room = [ORGANIC_PRODUCE => ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"],
+    CONVENTIONAL_PRODUCE => ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"]]
 end
 
 def sorted_matrix
